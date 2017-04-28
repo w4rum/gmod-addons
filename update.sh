373367b5879d3ed50c46b58c6c976760b93e720c
Tim Schmidt <w4rum@users.noreply.github.com>
2017-04-28 23:59:26 +0200

    built poisongun

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 addon_dir=$1
 addon_name=$(basename $addon_dir)
-addon_icon="$addon_dir$addonName.jpg"
+addon_icon="$addon_dir$addon_name.jpg"
 gmod_bin=$HOME/.steam/steam/steamapps/common/GarrysMod/bin
 workshop_id=$(cat "$addon_dir$addon_name.id")
 LD_LIBRARY_PATH=$gmod_bin $gmod_bin/gmpublish_linux update -id $workshop_id -addon "$addon_dir$addon_name.gma" -icon $addon_icon
