cfb0f14f90c9f8b49a1c175c1ca05456a01aa913
Tim Schmidt <w4rum@users.noreply.github.com>
2017-04-28 18:23:27 +0200

    Added poison dart gun by TFlippy and Infaredz

---
+++ ttt-poison-dart-gun/lua/autorun/tflippy_weaponpack.lua
@@ -0,0 +1,12 @@
+-- -- SOUND LIST -- --
+
+-- Neurotoxin Gun --
+
+sound.Add({
+	name = 			"Neurotoxin.TFlippy.Single",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		0.5,
+	level = 		70,
+	pitch =			{95, 105},
+	sound = 			"tflippy/dartgun/shoot.wav"
+})
