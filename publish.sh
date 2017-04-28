12343a0e19d96f19022927d78fadf111882e1aa2
Tim Schmidt <w4rum@users.noreply.github.com>
2017-04-28 22:26:38 +0200

    fixed bug in publish.sh and built poisongun

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 
 addon_dir=$1
 addon_name=$(basename $addon_dir)
-addon_icon="$addon_dir$addonName.jpg"
+addon_icon="$addon_dir$addon_name.jpg"
 gmod_bin=$HOME/.steam/steam/steamapps/common/GarrysMod/bin
-LD_LIBRARY_PATH=$gmod_bin $gmod_bin/gmpublish_linux create -addon $addon_dir$addon_name -icon $addon_icon
+LD_LIBRARY_PATH=$gmod_bin $gmod_bin/gmpublish_linux create -addon "$addon_dir$addon_name.gma" -icon $addon_icon
